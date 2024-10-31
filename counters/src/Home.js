c20c8e82dbef45029885de23b8b0a0c1a18b1ac1
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-10-31 11:43:23 +0100

    adding missing file

---
+++ counters/src/Home.js
@@ -0,0 +1,29 @@
+import Counter from "./Counter";
+import { useState } from "react";
+
+function Home() {
+  const [color, setColor] = useState("lightBlue");
+
+  return (
+    <div>
+      <h1>Hi from React</h1>
+
+      <br />
+
+      <Counter name={"Push Ups"} color={color} size={"24pt"} />
+
+      <Counter name={"Glasses of Water"} color={color} size={"14pt"} />
+
+      <button
+        onClick={() => {
+          setColor("yellow");
+        }}
+        type="submit"
+      >
+        Change Color
+      </button>
+    </div>
+  );
+}
+
+export default Home;
